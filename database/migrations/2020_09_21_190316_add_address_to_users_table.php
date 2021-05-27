8b5fdc3b1fe3845c02e9e3f87120f4f329adea28
simonet85 <christian.85@live.fr>
2021-05-27 10:18:31 +0200

    update user migration

--- database/migrations/2020_09_21_190316_add_address_to_users_table.php
+++ database/migrations/2020_09_21_190316_add_address_to_users_table.php
@@ -14,7 +14,7 @@ class AddAddressToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function($table) {
-            $table->string('address');
+            $table->string('address')->default('');
         });
     }
 
